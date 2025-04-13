d4f928690417049391d7fa1b64e819b56fabc4da
Ian Renton <ian@ianrenton.com>
2025-04-13 22:07:17 +0100

    Fix a bug where malformed grids would crash the software

--- js/display-funcs.js
+++ js/display-funcs.js
@@ -124,7 +124,7 @@ function getTooltipText(qso) {
 // path.
 function getIconPosition(q) {
     let grid = q.get("GRIDSQUARE");
-    if (grid) {
+    if (grid && latLonForGrid(grid) != null) {
         [lat, lon] = latLonForGrid(grid);
         if (lat != null && lon != null && !isNaN(lat) && !isNaN(lon) && (lat !== 0.0 || lon !== 0.0)) {
             let wrapEitherSideOfLon = 0;
