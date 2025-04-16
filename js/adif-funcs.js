8b9a1b735cb015c64e56c1c69c89ac0f1a58967d
Ian Renton <ian@ianrenton.com>
2025-04-16 19:23:06 +0100

    QRZ lookups add to the data map. #9

--- js/adif-funcs.js
+++ js/adif-funcs.js
@@ -2,6 +2,31 @@
 //   ADIF LOAD FUNCTIONS   //
 /////////////////////////////
 
+// Put a QSO into the main data map. It must have a callsign and grid at this point. This can be called either from
+// loadAdif() if the QSO is fully populated in ADIF, or from processQSOFromQueue() if the QSO was in the queue and we
+// now have a lookup successfully returning a grid.
+function putQSOIntoDataMap(qso) {
+    // The data map is keyed by "CALL-GRID" so we do not duplicate the combination. If there has not
+    // been a QSO with this combination before, we need to create it; we also add the "call", "grid"
+    // properties at the top level for easier lookup. But if we have seen the combination before, this
+    // is a dupe, and we just add the new qso to the existing qsos list.
+    let key = qso.call + "-" + qso.grid;
+    if (data.has(key)) {
+        data.get(key).qsos.push(qso);
+    } else {
+        data.set(key, {qsos: [qso], call: qso.call, grid: qso.grid});
+    }
+
+    // If we haven't seen a name or a QTH description for this call & grid before, add them at the top
+    // level, to avoid display functions having to iterate through qsos to find this.
+    if (!data.get(key).name && qso.name) {
+        data.get(key).name = qso.name;
+    }
+    if (!data.get(key).qth && qso.qth) {
+        data.get(key).qth = qso.qth;
+    }
+}
+
 // Given the text of an adif file, populate the qsos map.
 function loadAdif(text) {
     data = new Map();
@@ -63,61 +88,43 @@ function loadAdif(text) {
                 }
 
                 // All the fields have been extracted into qsoData. Now turn them into a QSO object for storage.
-                let qso = {call: qsoData.get("CALL")};
-                if (qsoData.has("NAME")) {
-                    qso.name = qsoData.get("NAME");
-                }
-                if (qsoData.has("GRIDSQUARE")) {
-                    qso.grid = qsoData.get("GRIDSQUARE").toUpperCase();
-                }
-                if (qsoData.has("QTH")) {
-                    qso.qth = qsoData.get("QTH");
-                }
-                if (qsoData.has("FREQ")) {
-                    qso.freq = parseFloat(qsoData.get("FREQ"));
-                }
-                if (qsoData.has("MODE")) {
-                    qso.mode = qsoData.get("MODE");
-                }
-                if (qsoData.has("QSO_DATE") && qsoData.has("TIME_ON")) {
-                    qso.time = moment.utc(qsoData.get("QSO_DATE") + qsoData.get("TIME_ON").substring(0, 4), "YYYYMMDDHHmm");
-                }
-                if (qsoData.has("SIG")) {
-                    qso.sig = qsoData.get("SIG");
-                }
+                if (qsoData.has("CALL")) {
+                    let qso = {call: qsoData.get("CALL")};
+                    if (qsoData.has("NAME")) {
+                        qso.name = qsoData.get("NAME");
+                    }
+                    if (qsoData.has("GRIDSQUARE")) {
+                        qso.grid = qsoData.get("GRIDSQUARE").toUpperCase();
+                    }
+                    if (qsoData.has("QTH")) {
+                        qso.qth = qsoData.get("QTH");
+                    }
+                    if (qsoData.has("FREQ")) {
+                        qso.freq = parseFloat(qsoData.get("FREQ"));
+                    }
+                    if (qsoData.has("MODE")) {
+                        qso.mode = qsoData.get("MODE");
+                    }
+                    if (qsoData.has("QSO_DATE") && qsoData.has("TIME_ON")) {
+                        qso.time = moment.utc(qsoData.get("QSO_DATE") + qsoData.get("TIME_ON").substring(0, 4), "YYYYMMDDHHmm");
+                    }
+                    if (qsoData.has("SIG")) {
+                        qso.sig = qsoData.get("SIG");
+                    }
 
-                // If the QSO has a grid, we can put it straight into the data map and it will be displayed immediately
-                // once we have finished parsing the ADIF.
-                if (qso.grid) {
-                    // The data map is keyed by "CALL-GRID" so we do not duplicate the combination. If there has not
-                    // been a QSO with this combination before, we need to create it; we also add the "call", "grid"
-                    // properties at the top level for easier lookup. But if we have seen the combination before, this
-                    // is a dupe, and we just add the new qso to the existing qsos list.
-                    let key = qso.call + "-" + qso.grid;
-                    if (data.has(key)) {
-                        data.get(key).qsos.push(qso);
+                    if (qso.grid) {
+                        // If the QSO has a grid, we can put it straight into the data map and it will be displayed immediately
+                        // once we have finished parsing the ADIF.
+                        putQSOIntoDataMap(qso);
                     } else {
-                        data.set(key, {qsos: [qso], call: qso.call, grid: qso.grid});
+                        // The QSO has no grid, so we need to look it up. We place it in a queue, it will be dealt with
+                        // later asynchronously.
+                        queue.push(qso);
                     }
 
-                    // If we haven't seen a name or a QTH description for this call & grid before, add them at the top
-                    // level, to avoid display functions having to iterate through qsos to find this.
-                    if (!data.get(key).name && qso.name) {
-                        data.get(key).name = qso.name;
-                    }
-                    if (!data.get(key).qth && qso.qth) {
-                        data.get(key).qth = qso.qth;
-                    }
-
-                } else {
-                    // The QSO has no grid, so we need to look it up. We place it in a queue, it will be dealt with
-                    // later asynchronously.
-                    // todo change to map indexed by callsign
-                    queue.push(qso);
+                    // Increment counter
+                    qsoCount++;
                 }
-
-                // Increment counter
-                qsoCount++;
             }
 
             // Update the map
@@ -140,31 +147,56 @@ function loadAdif(text) {
 function processQSOFromQueue() {
     // todo cache save & restore, do >1 process per tick if we are only loading from local store?
     if (queue.length > 0 && qrzToken) {
+        // Pop the next QSO out of the queue
         let qso = queue.pop();
-        $.ajax({
-            url: QRZ_API_BASE_URL,
-            data: {s: qrzToken, callsign: encodeURI(qso.call), agent: QRZ_AGENT},
-            dataType: 'xml',
-            timeout: 10000,
-            success: async function (result) {
-                let grid = $(result).find("grid");
-                if (grid && grid.text().length > 0) {
-                    console.log(grid.text());
-                    // todo get fname+name, something for qth? (see what PoLo does?)
-                    // todo move qso into data map
+        if (qso.call) {
+            $.ajax({
+                url: QRZ_API_BASE_URL,
+                data: {s: qrzToken, callsign: encodeURI(qso.call), agent: QRZ_AGENT},
+                dataType: 'xml',
+                timeout: 10000,
+                success: async function (result) {
+                    let grid = $(result).find("grid");
+                    if (grid && grid.text().length > 0) {
+                        // We have a good grid, so assign it to the QSO to make it ready to go into the data map.
+                        qso.grid = grid.text();
 
-                    // Update the map
-                    // todo is doing this every second too much? Maybe every n, or in a separate thread every 10 sec if something has changed?
-                    updateMapObjects();
-                } else {
-                    // todo see what we are getting here, do we get lat/lon and not grid maybe? Or no position at all?
-                    // todo how to handle failed lookups & display this
+                        // Get some name & QTH information for the QSO if we have it
+                        let fname = $(result).find("fname");
+                        let name = $(result).find("name");
+                        let country = $(result).find("country");
+                        let nameJoined = "";
+                        if (fname && fname.text().length > 0) {
+                            nameJoined += fname.text();
+                        }
+                        if (name && name.text().length > 0) {
+                            nameJoined += " " + name.text();
+                        }
+                        qso.name = nameJoined;
+                        if (country && country.text().length > 0) {
+                            qso.qth = country.text();
+                        }
+
+                        // Update the data map
+                        putQSOIntoDataMap(qso);
+
+                        // Update the (geographic) map
+                        updateMapObjects();
+                    } else {
+                        // No grid in response or call is not in the QRZ database.
+                        failedLookupCount++;
+                        console.log("QRZ.com had no grid for " + qso.call);
+                    }
+                },
+                error: function () {
+                    failedLookupCount++;
+                    console.log("Error from QRZ.com when looking up " + qso.call);
                 }
-            },
-            error: function () {
-                // todo how to handle failed lookups & display this
-            }
-        });
+            });
+        } else {
+            // QSO had no call, nothing to look up!
+            failedLookupCount++;
+        }
     }
 }
 
@@ -193,7 +225,7 @@ function updateStatus() {
                 statusText += "Loaded " + qsoCount + " QSOs, failed to find grids for " + failedLookupCount + ".";
             } else if (failedLookupCount === 0) {
                 if (qrzToken) {
-                    statusText += "Loaded " + qsoCount + " QSOs, " + failedLookupCount + " in lookup queue.";
+                    statusText += "Loaded " + qsoCount + " QSOs, " + queue.length + " in lookup queue.";
                 } else {
                     statusText += "Loaded " + qsoCount + " QSOs, " + queue.length + " had no grid.";
                 }
